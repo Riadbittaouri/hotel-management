e79c7fe680403d24b2bc2cf4567a48d1c59fa748
Riadbittaouri <bittaouri.r@hotmail.com>
2023-08-20 18:13:04 +0100

    7 commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                         Overview
                     </div>
                     <div class="card-body">
-                        <p>No activity found.</p>
+                      <p> Welcome to the Admin Dashboard! This section provides a snapshot of key statistics and insights related to user activities and engagement. You can quickly see the total number of users registered, the total logins recorded, and the average number of logins per day. Additionally, we highlight the number of new users who joined this month and the total active users interacting with the platform. As an admin, you can gain insights into user engagement through the average activity per user metric. Explore further to analyze user behavior and make informed decisions to enhance user experience.</p>   
                     </div>
                 </div>
             </div>
@@ -21,34 +21,32 @@
                     <div class="card-header">
                         Recent Activities
                     </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Date</th>
-                                        <th>Time</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach (auth()->user()->activities as $activity)
-                                        @if (!$activity->user->is_admin)
-                                            <tr>
-                                                <td>{{ $activity->user->name }}</td>
-                                                <td>{{ $activity->created_at->format('Y-m-d') }}</td>
-                                                <td>{{ $activity->created_at->format('H:i:s') }}</td>
-                                            </tr>
-                                        @endif
-                                    @endforeach
-                                    @if (auth()->user()->activities->where('user.is_admin', 0)->isEmpty())
+                    <div class="card-body" style="height: 300px; overflow-y: auto;">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach (auth()->user()->activities as $activity)
+                                    @if (!$activity->user->is_admin)
                                         <tr>
-                                            <td colspan="3">No activity found.</td>
+                                            <td>{{ $activity->user->name }}</td>
+                                            <td>{{ $activity->created_at->format('Y-m-d') }}</td>
+                                            <td>{{ $activity->created_at->format('H:i:s') }}</td>
                                         </tr>
                                     @endif
-                                </tbody>
-                            </table>
-                        </div>
+                                @endforeach
+                                @if (auth()->user()->activities->where('user.is_admin', 0)->isEmpty())
+                                    <tr>
+                                        <td colspan="3">No activity found.</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -61,7 +59,14 @@
                         Admin Statistics
                     </div>
                     <div class="card-body">
-                        <canvas id="admin-stats-chart"></canvas>
+                        <ul>
+                            <li>Total Users: {{ App\Models\User::count() }}</li>
+                            <li>Total Logins: {{ App\Models\UserActivity::count() }}</li>
+                            <li>Average Logins per Day: {{ round(App\Models\UserActivity::count() / (strtotime('now') - strtotime('first day of this month')) * 86400, 2) }}</li>
+                            <li>New Users This Month: {{ App\Models\User::whereMonth('created_at', now()->month)->count() }}</li>
+                            <li>Total Active Users: {{ App\Models\UserActivity::distinct('user_id')->count() }}</li>
+                            <li>Average Activity per User: {{ round(App\Models\UserActivity::count() / App\Models\UserActivity::distinct('user_id')->count(), 2) }}</li>
+                        </ul>
                     </div>
                 </div>
             </div>
